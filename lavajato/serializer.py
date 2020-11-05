07e903d9d4822530674a8d28f7ae23e00f0d6252
jhoneffernandes <49643971+jhoneffernandes@users.noreply.github.com>
2020-11-05 10:20:29 -0300

    Tarefa do dia 05/11

--- lavajato/serializer.py
+++ lavajato/serializer.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from lavajato.models import Cliente, Veiculo
 
+
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = ['id', 'nome', 'email', 'cpf', 'telefone']
 
+
 class VeiculoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Veiculo
-        field = ['id', 'placa', 'ano', 'modelo', 'cor', 'descricao']
+        fields = ['id', 'placa', 'ano', 'modelo', 'cor', 'descricao', 'cliente']
--- lavajato/views.py
+++ lavajato/views.py
@@ -5,11 +5,9 @@ from lavajato.serializer import ClienteSerializer, VeiculoSerializer
 from django.http import JsonResponse
 
 class ClientesViewSet(viewsets.ModelViewSet):
-    """Exibindo todos os alunos e alunas"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
 class VeiculosViewSet(viewsets.ModelViewSet):
-    """Exibindo todos os cursos"""
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
