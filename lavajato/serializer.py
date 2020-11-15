30ff9e0171815c9375d4c6337ff64dc6cd038645
jhoneffernandes <49643971+jhoneffernandes@users.noreply.github.com>
2020-11-15 18:33:14 -0300

    Tarefa do dia 12/11

--- lavajato/serializer.py
+++ lavajato/serializer.py
@@ -12,3 +12,16 @@ class VeiculoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Veiculo
         fields = ['id', 'placa', 'ano', 'modelo', 'cor', 'descricao', 'cliente']
+
+class ListaVeiculosClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ['placa', 'modelo']
+
+class ListaClientesCadastradosVeiculoSerializer(serializers.ModelSerializer):
+    cliente_id = serializers.ReadOnlyField(source='cliente.id')
+    cliente_nome = serializers.ReadOnlyField(source='cliente.nome')
+    cliente_cpf = serializers.ReadOnlyField(source='cliente.cpf')
+    class Meta:
+        model = Veiculo
+        fields = ['cliente_id', 'cliente_nome', 'cliente_cpf']
--- lavajato/views.py
+++ lavajato/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from lavajato.models import Cliente, Veiculo
 from lavajato.serializer import ClienteSerializer, VeiculoSerializer
+from rest_framework import viewsets, generics
+from lavajato.serializer import ClienteSerializer, VeiculoSerializer, ListaVeiculosClienteSerializer, ListaClientesCadastradosVeiculoSerializer
 
 from django.http import JsonResponse
 
@@ -11,3 +13,18 @@ class ClientesViewSet(viewsets.ModelViewSet):
 class VeiculosViewSet(viewsets.ModelViewSet):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+
+
+class ListaVeiculosClienteViewSet(generics.ListAPIView):
+    """Exibindo as matriculas de um aluno"""
+    def get_queryset(self):
+        queryset = Veiculo.objects.filter(cliente_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaVeiculosClienteSerializer
+
+class ListaClientesCadastradosVeiculoViewset(generics.ListAPIView):
+    """Exibindo alunos matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Veiculo.objects.filter(id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaVeiculosClienteSerializer
